9191e3bc67d2aa7123f981aa986989dbc44df905
Ken Chan <kenchan12348@gmail.com>
2024-02-27 18:24:17 -0500

    Add React Router

---
+++ src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import { Typography } from '@mui/material'
+import { Box } from '@mui/system'
+
+function Home () {
+  return (
+    <Box>
+      <Typography variant="h1" align="center" color="primary">
+        Home
+      </Typography>
+    </Box>
+  )
+}
+
+export default Home
