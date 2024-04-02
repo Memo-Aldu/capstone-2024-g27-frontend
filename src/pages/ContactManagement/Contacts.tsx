ae03011dc2cff50f71d44ed450261030008b3d61
jean232 <ancho057@uottawa.ca>
2024-04-01 21:31:54 -0400

    modification de la page contact

--- src/pages/ContactManagement/Contacts.tsx
+++ src/pages/ContactManagement/Contacts.tsx
@@ -1,13 +1,51 @@
-import { Typography } from '@mui/material'
+import { Box, Button, Grid, Typography } from '@mui/material'
 import ContactManagementLayout from './ContactManagementLayout'
+import { useState } from 'react';
+import ContactForm from './ContactForm';
+import { contactsReducer, State } from './ContactReducer';
+import { useReducer } from 'react';
+import ContactList from './ContactList';
 
+const initialState: State = {
+  contacts: []
+};
 function Contacts (): JSX.Element {
+  const [state, dispatch] = useReducer(contactsReducer, initialState);
+
+  const [isPopupOpen, setIsPopupOpen] = useState(false); // État pour contrôler l'ouverture/fermeture de la popup
+
+  // Fonction pour basculer l'état de la popup entre visible et invisible
+  function togglePopup() {
+    setIsPopupOpen(!isPopupOpen);
+  }
+
+
   return (
-    <ContactManagementLayout>
-        <Typography variant="h4" component="h1" gutterBottom>
-          Contacts
-        </Typography>
-    </ContactManagementLayout>
+    
+      <ContactManagementLayout>
+        <Grid container spacing={2}>
+          <Grid item xs={8}>
+              <Typography variant="h4" component="h1" gutterBottom>
+              Contact list
+              </Typography>
+          </Grid>
+          <Grid item xs={4}>
+              <Button variant="contained" color="primary" onClick={togglePopup} style={{ float: 'right' }}>
+              {isPopupOpen ? 'Close' : 'Add Contact'}
+              </Button>
+          </Grid>
+          <Grid item xs={12}>
+            {/* Affichage du formulaire de contact uniquement si la popup est ouverte */}
+            {isPopupOpen && <ContactForm dispatch={dispatch} onClose={togglePopup} />}
+          </Grid>
+          <Grid item xs={12}>
+          {state.contacts.length > 0 && <ContactList contacts={state.contacts} />}
+          </Grid>
+        </Grid>    
+
+          
+      </ContactManagementLayout>
+
   )
 }
 
