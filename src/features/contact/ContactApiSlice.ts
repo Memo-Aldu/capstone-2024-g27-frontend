b847a18cb74c5597cdd8201f462c0e6c4cbf5c94
Ken Chan <kenchan12348@gmail.com>
2024-04-10 16:38:56 -0400

    Add Tags to ContactService

--- src/features/contact/ContactApiSlice.ts
+++ src/features/contact/ContactApiSlice.ts
@@ -7,39 +7,45 @@ const contactApiSlice = ContactApiSlice.injectEndpoints({
       query: (id: string) => ({
         url: `/${id}`,
         method: 'GET'
-      })
+      }),
+      providesTags: ['Contact']
     }),
     getAllContacts: builder.query<Contact[], void>({
       query: () => ({
         url: '',
         method: 'GET'
-      })
+      }),
+      providesTags: ['Contact']
     }),
     createContact: builder.mutation<Contact, Contact>({
       query: (contact: Contact) => ({
         url: '',
         method: 'POST',
         body: contact
-      })
+      }),
+      invalidatesTags: ['Contact']
     }),
     updateContact: builder.mutation<Contact, Contact>({
       query: (contact: Contact) => ({
         url: `/${contact.id}`,
         method: 'PATCH',
         body: contact
-      })
+      }),
+      invalidatesTags: ['Contact']
     }),
     deleteContact: builder.mutation<void, string>({
       query: (id: string) => ({
         url: `/${id}`,
         method: 'DELETE'
-      })
+      }),
+      invalidatesTags: ['Contact']
     }),
     searchContactsByName: builder.query<Contact[], string>({
       query: (name: string) => ({
         url: `/search/${name}`,
         method: 'GET'
-      })
+      }),
+      providesTags: ['Contact']
     })
   })
 })
